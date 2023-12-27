71b21a89203c727191650f2d6ae861991078971c
sanu1020 <sanuuthaya02@gmail.com>
2023-12-27 11:08:26 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Footer from './components/Footer';
 import Intro from './components/intro';
 import Portfolio from './components/Portfolio';
 import Timeline from './components/Timeline';
-import Commits from './components/Github/commits';
 
 function App() {
 	const [theme, setTheme] = useState(null);
@@ -76,7 +75,7 @@ function App() {
 			<div className="max-w-5xl w-11/12 mx-auto">
 				<Intro />
 				<Portfolio />
-        <Timeline />
+        		<Timeline />
 				<Contact />
 				<Footer />
 			</div>
